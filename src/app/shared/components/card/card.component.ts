2248ab2e90fda979e67932b5dafc6ecba0d55d0b
rafaelfreitas <rafa.oliveirafreitas@hotmail.com>
2019-02-10 23:22:33 -0200

    Componentizando filter

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,9 @@
+import { Input, Component } from "@angular/core";
+
+@Component({
+    selector: 'gp-card',
+    templateUrl: './card.component.html'
+})
+export class CardComponent {
+    @Input() title: string = "";
+}
