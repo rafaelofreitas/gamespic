ce6c66bd7e0340d261e84356212c2e133c17b387
rafaelfreitas <rafa.oliveirafreitas@hotmail.com>
2019-02-12 23:24:25 -0200

    autenticacao token, cabecalho da aplicacao

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators'
+
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -8,9 +11,15 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private tokenService: TokenService,
+    private http: HttpClient) { }
 
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + '/user/login', { userName, password })
+    return this.http.post(API_URL + '/user/login', { userName, password }, { observe: 'response' })
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-acess-token');
+        this.tokenService.setToken(authToken);
+      }))
   }
 }
