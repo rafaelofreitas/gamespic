3150c08b3f9e178a10288aa527f2e67453d1df71
rafaelfreitas <rafa.oliveirafreitas@hotmail.com>
2019-02-13 00:12:57 -0200

    descriptografando token, verificacao usuario logado

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators'
 
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -12,14 +12,19 @@ const API_URL = 'http://localhost:3000';
 export class AuthService {
 
   constructor(
-    private tokenService: TokenService,
+    private userService: UserService,
     private http: HttpClient) { }
 
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + '/user/login', { userName, password }, { observe: 'response' })
+    return this.http.post(
+      API_URL + '/user/login',
+      { userName, password },
+      { observe: 'response' }
+    )
       .pipe(tap(res => {
-        const authToken = res.headers.get('x-acess-token');
-        this.tokenService.setToken(authToken);
+        const authToken = res.headers.get('x-access-token');
+        this.userService.setToken(authToken);
+        console.log(`Usuario ${userName} autenticado com token ${authToken}`);
       }))
   }
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from "@angular/core";
+import { UserService } from "../user/user.service";
+import { Observable } from "rxjs";
+import { User } from "../user/user";
 
 @Component({
     selector: 'gp-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent { 
-    @Input() userName: string;
+export class HeaderComponent {
+
+    user$: Observable<User>;
+    user: User;
+
+    constructor(userService: UserService) {
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
 }
