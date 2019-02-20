7543ab2d078cae763c16df48b5893f1213882733
rafaelfreitas <rafa.oliveirafreitas@hotmail.com>
2019-02-20 20:36:42 -0300

    criacao de formulario p/ registro de novos usuarios!

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { SignInComponent } from "./home/signin/signin.component";
 import { AuthGuard } from "./core/auth/auth.guard";
+import { SignUpComponent } from "./home/signup/signup.component";
 
 const routes: Routes = [
     {
@@ -14,6 +15,12 @@ const routes: Routes = [
         component: SignInComponent,
         canActivate: [AuthGuard]
     },
+
+    {
+        path: 'signup',
+        component: SignUpComponent,
+    },
+
     {
         path: 'user/:userName',
         component: PhotoListComponent,
