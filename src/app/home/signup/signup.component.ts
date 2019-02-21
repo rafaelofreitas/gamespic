a2d4502309d0c8da243acfdffe0c625584e38327
rafaelfreitas <rafa.oliveirafreitas@hotmail.com>
2019-02-20 21:16:44 -0300

    validador assincrono para registro de novos usuarios

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { UserNotTakenValidatorService } from "./user-not-taken.validator.service";
 
 @Component({
     templateUrl: './signup.component.html'
@@ -8,9 +9,13 @@ export class SignUpComponent implements OnInit {
 
     signupForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private userNotTakenValidatorService: UserNotTakenValidatorService
+    ) { }
 
     ngOnInit(): void {
+        const fn = this.userNotTakenValidatorService.checkUserNameTaken();
         this.signupForm = this.formBuilder.group({
             email: ['',
                 [
@@ -31,7 +36,8 @@ export class SignUpComponent implements OnInit {
                     Validators.pattern(/^[a-z0-9\-]+$/),
                     Validators.minLength(2),
                     Validators.maxLength(30)
-                ]
+                ],
+                this.userNotTakenValidatorService.checkUserNameTaken()
             ],
             password: ['',
                 [
